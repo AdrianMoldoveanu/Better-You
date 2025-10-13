0e7fcd3091bd3a70ad9de79e810d0831e52e6fce
AdrianMoldoveanu <73549885+AdrianMoldoveanu@users.noreply.github.com>
2025-10-13 23:08:00 +0000

    change buttons

--- timer.js
+++ timer.js
@@ -2,15 +2,20 @@ document.querySelectorAll('.timer').forEach(button => {
     button.addEventListener('click', function () {
         let seconds = 30;
         button.disabled = true;
+        button.classList.add('vh-100');
+        button.style.color = 'red';
+        button.style.backgroundColor = 'black';
+    button.style.fontSize = '9rem';
         const originalText = button.textContent;
         button.textContent = `${seconds}s`;
-
+       
         const interval = setInterval(() => {
             seconds--;
             button.textContent = `${seconds}s`;
             if (seconds <= 0) {
                 clearInterval(interval);
                 button.textContent = originalText;
+                button.classList.remove('vh-100')
                 button.disabled = false;
             }
         }, 1000);
