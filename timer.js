4a7a95c4fd14252a6dd4a7c3ed07fc5f336c1dab
AdrianMoldoveanu <73549885+AdrianMoldoveanu@users.noreply.github.com>
2025-10-14 20:03:24 +0000

    Add font family

--- timer.js
+++ timer.js
@@ -6,10 +6,10 @@ document.querySelectorAll('.timer').forEach(button => {
         button.classList.add('vw-100');
         button.style.color = '#0ef506ff';
         button.style.backgroundColor = 'black';
-    button.style.fontSize = '7rem';
+        button.style.fontSize = '7rem';
         const originalText = button.textContent;
         button.textContent = `${seconds}s`;
-       
+
         const interval = setInterval(() => {
             seconds--;
             button.textContent = `${seconds}s`;
